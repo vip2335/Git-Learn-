21f0108fb1776e202edc57fc9ef23ccc9bf9cc51
Vishal Parmar <vparmar@isystango.com>
2021-11-17 12:04:36 +0530

    add method setUp

--- GitData/ViewController.swift
+++ GitData/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("hello world")
+        setUp()
+        
         // Do any additional setup after loading the view.
     }
-
+func setUp()
+    {
+        print("hi github")
+    }
 
 }
 
